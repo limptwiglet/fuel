204db8a45dbe041228f8f29f5ea61dfe0a9d4fc3
Mark Gerrard <limptwiglet@gmail.com>
2012-03-25 20:42:28 +0100

    Updated tests for building, still need to mock local files

--- test/build.js
+++ test/build.js
@@ -6,7 +6,7 @@ var expect = require('chai').expect;
 
 var appPath = './exampleApp/';
 
-describe('build tool', function () {
+describe('build', function () {
 	describe('getConfig', function () {
 		it('should not find a config', function (done) {
 			build.getConfig('../', function (err, conf) {
@@ -51,51 +51,30 @@ describe('build tool', function () {
 	});
 
 	describe('buildFile', function () {
-		it('should accept file type and file data', function (done) {
-			build.buildFile({
-				type: 'javascript',
-				data: '{}'
-			}, function (err, file) {
-				expect(err).to.not.exist;
-				expect(file).to.be.a('string');
-				done();
-			});
-		});
-
-		it('should throw an error on unhandable file types', function (done) {
-			build.buildFile({
-				type: 'adfadsfadsf',
-				data: '{}'
-			}, function (err, file) {
+		it('should throw an error if no file found', function (done) {
+			build.buildFile('./bur.js', function (err, file) {
 				expect(err).to.exist;	
-				expect(file).to.not.exist;
 				done();
 			});
 		});
 
-		it('should call _getFiles to get paths', function () {
-			build.buildFile({
-				type: 'javascript',
-				path: appPath + 'main.js'
-			}, function (err, file) {
-				expect(err).to.not.exist;	
-				expect(file).to.be.a('string');
+		it('should throw and error if the file cannot be built', function (done) {
+			build.buildFile('./bur.flanger', function (err, file) {
+				expect(err).to.exist;
+				expect(err.message).to.equal('Invalid file extension');
 				done();
 			});
 		});
+	});
 
-		it('should wrap template files', function (done) {
-			build.buildFile({
-				type: 'html',
-				fileName: 'test',
-				data: '<div id=""></div>'
-			}, function (err, file) {
-				expect(err).to.not.exist;	
-				expect(file).to.be.a('string');
-				expect(file).to.match(/^Ember\.TEMPLATES\[\'test\'\] \= Ember\.Handlebars\.compile\(".+"\);$/);
+
+	describe('buildDirectory', function () {
+		it('should top and tail output with setup.js and init.js', function (done) {
+			build.buildDirectory('./exampleApp/', function (err, data) {
+				data = data.replace(/\n/g, '');
+				expect(err).to.not.exist;
 				done();
 			});
 		});
-
 	});
 });
